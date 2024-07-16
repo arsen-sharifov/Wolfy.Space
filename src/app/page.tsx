f1e91535a17d48d161efbb9107dcdb04f64abb95
Arsen Sharifov <sharifov.arsen.0@gmail.com>
2024-07-17 02:02:52 +0300

    WOSP-2: Create Welcome Page, basic layout was added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import "./styles/style.css";
+
 export default function Welcome() {
   return (
-   <div>Welcome to Wolfy.Space</div>
+   <div className="wrapper">
+    <div>
+      <h2>Welcome to Wolfy.space!</h2>
+      <h2>Customize your profile, share posts, play games, and connect with friends in the ultimate social network experience</h2>
+      <div className="container">
+        <div className="header">
+          <span className="signIn">Sign In</span>
+          <span className="signUp">Sign Up</span>
+        </div>
+        <div className="form">
+          <input type="text" placeholder="Login" className="input" />
+          <input type="password" placeholder="Password" className="input" />
+          <button className="button">Go!</button>
+        </div>
+      </div>
+    </div>
+    <div><img src='/logo.png' alt="Logo" width={544} height={674}></img></div>
+  </div>
   );
 }
