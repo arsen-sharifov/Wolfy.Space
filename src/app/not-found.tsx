1d4c3b0a09c325fa017df53942c96fbc9169735a
Arsen <sharifov.arsen.0@gmail.com>
2024-07-30 22:33:36 +0300

    WOSP-22: Add Custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-primary-color text-center">
+      <h1 className="text-9xl font-bold text-text-color">404</h1>
+      <p className="mt-4 text-2xl text-text-color">Page Not Found</p>
+    </div>
+  );
+};
+
+export default NotFound;
