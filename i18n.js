d0427fdec1073b4212369f74f98125ca3284152e
Sharifov Arsen <111392862+Sharifov-Arsen@users.noreply.github.com>
2024-08-06 16:34:55 +0300

    WOSP-34: FE. Fix error at the left bottom corner (#57)
    * WOSP-34: FE. Fix error at the left bottom corner
    
    * WOSP-34: FE. Fix error at the left bottom corner

--- i18n.js
+++ i18n.js
@@ -1,50 +1,28 @@
 'use client';
-
+import { useEffect } from 'react';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from './public/locales/en/common.json';
-import pl from './public/locales/pl/common.json';
-import ua from './public/locales/ua/common.json';
-import de from './public/locales/de/common.json';
-import pt from './public/locales/pt/common.json';
-import fr from './public/locales/fr/common.json';
-import { useEffect } from 'react';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-const resources = {
-  en: {
-    common: en,
-  },
-  ua: {
-    common: ua,
-  },
-  pl: {
-    common: pl,
-  },
-  de: {
-    common: de,
-  },
-  pt: {
-    common: pt,
-  },
-  fr: {
-    common: fr,
-  },
-};
-
-let savedLanguage = 'en';
-
-if (typeof window !== 'undefined') {
-  savedLanguage = localStorage.getItem('language') || 'en';
-}
-
-i18n.use(initReactI18next).init({
-  resources,
-  lng: savedLanguage,
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'ua', 'pl', 'de', 'pt', 'fr'],
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+  });
 
 export function I18nInit() {
   useEffect(() => {
